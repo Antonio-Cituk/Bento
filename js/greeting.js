a70767b01b514043d1662e0fd007df74a7d34b5c
Antonio Cituk <tonycituk@gmail.com>
2020-12-12 01:20:30 -0500

    ajustes 👽

--- js/greeting.js
+++ js/greeting.js
@@ -4,17 +4,17 @@ var hour = today.getHours();
 
 // Here you can change your name
 var name = 'Tony';
-var emojis = ['👽', '👾', '🤖', '🎃', '😺'];
+var emojis = ['👽', '👾', '🤖', '😺', '👻', '🌈', '💗', '🧠', '🚀'];
 
 function randomEmoji(){
   return emojis[Math.floor(Math.random() * emojis.length)];
 }
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep ';
-var gree2 = '¡Good morning ';
-var gree3 = 'Good afternoon ';
-var gree4 = '¡Ya bañate ';
+var gree1 = 'Go to sleep ';
+var gree2 = 'Good morning ';
+var gree3 = 'Be happy! ';
+var gree4 = 'Hey, you can do it!';
 var gree5 = 'Good evening ';
 var gree6 = 'Good evening ';
 
@@ -22,9 +22,9 @@ var gree6 = 'Good evening ';
 if (hour >= 23 && hour < 5) {
     document.getElementById('greetings').innerText = gree1 + name + "! " + randomEmoji();
 } else if (hour >= 6 && hour < 12) {
-    document.getElementById('greetings').innerText = gree2 + name;
+    document.getElementById('greetings').innerText = gree2 + name + "! " + randomEmoji();
 } else if (hour >= 12 && hour < 17) {
-    document.getElementById('greetings').innerText = gree3 + name;
+    document.getElementById('greetings').innerText = gree3 + '💗';
 } else  {
-    document.getElementById('greetings').innerText = gree4 + name;
+    document.getElementById('greetings').innerText = gree4 + randomEmoji();
 }
--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '8d9bfd5e3979b0e98203376eb933fe6c';
 
 // Set Position function
 setPosition();
